6a8c5b9e435abba2aa2b9ec972b74d8aabd25e4c
dhruyan baldaniya <baldaniyadhruyan3@gmail.com>
2023-10-02 10:57:31 +0530

    change

--- lib/utils/app_route.dart
+++ lib/utils/app_route.dart
@@ -3,6 +3,8 @@ import 'package:getx_app/utils/routes_name.dart';
 import 'package:getx_app/view/home/home_bindings.dart';
 
 import '../view/home/home_view.dart';
+import '../view/splash/splash_bindings.dart';
+import '../view/splash/splash_view.dart';
 
 class AppRoutes {
   static List<GetPage<dynamic>>? getPages = [
